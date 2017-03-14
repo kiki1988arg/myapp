71181484296c314205c499aac7c8034eabcd11f7
kiki1988arg <ezequielarakaki@hotmail.com>
2017-03-14 13:47:29 -0300

    Nunjucks!

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var nunjucks  = require('nunjucks');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,14 +6,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express   : app
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', {
+    title : 'My First Nunjucks Page',
+    items : [
+      { name : 'item #1' },
+      { name : 'item #2' },
+      { name : 'item #3' },
+      { name : 'item #4' },
+    ]
+  });
 });
 
 module.exports = router;
