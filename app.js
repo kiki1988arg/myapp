af3409d15fc7bbfb4fc752cee32a4a21fcc004ca
Nicolas Buzzi <xxneecoxx@gmail.com>
2017-03-16 18:16:19 -0300

    Socket

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-var nunjucks  = require('nunjucks');
+var nunjucks = require('nunjucks');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 
 var index = require('./routes/index.js');
 var users = require('./routes/users.js');
-var users = require('./routes/users.js');
+var chat = require('./routes/chat.js');
 
 
-var app = express();
+
+//chat
+var messages = [{
+  author: "Carlos",
+  text: "Hola! que tal?"
+}, {
+  author: "Pepe",
+  text: "Muy bien! y tu??"
+}, {
+  author: "Paco",
+  text: "Genial!"
+}];
+
+io.on('connection', function (socket) {
+  console.log('Un cliente se ha conectado');
+  socket.emit('messages', messages);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,30 +44,38 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/socket', express.static(__dirname + '/node_modules/socket.io-client/dist'));
 nunjucks.configure('views', {
   autoescape: true,
-  express   : app
+  express: app
 });
 
 app.use('/', index);
-
 app.use('/users', users);
+app.use('/chat', chat);
+
+
+
+
+
+
+
+
 
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-  
+
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
